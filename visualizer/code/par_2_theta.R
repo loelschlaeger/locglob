52c7b7c77dfcfb2321524db4469378673a24dcf7
Lennart <lennart.oelschlaeger@uni-bielefeld.de>
2021-08-12 21:48:46 +0200

    deleted visualizer code

--- visualizer/code/par_2_theta.R
+++
@@ -1,7 +0,0 @@
-### transformation of par to theta
-### par: list of all model parameters
-
-par_2_theta = function(par) {
-  theta = list(b = par$b, o = par$O[lower.tri(par$O,diag=TRUE)], l = par$L[lower.tri(par$L,diag=TRUE)][-1])
-  return(theta)
-}
