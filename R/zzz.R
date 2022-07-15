7ecb40674fe708b554c389264992c3fbc9182ab1
loelschlaeger <lennart.oelschlaeger@uni-bielefeld.de>
2022-07-15 17:24:58 +0200

    Update.

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,11 @@
-.onAttach = function(lib, pkg) {
-  # startup message
-  msg = paste0(
-    "Thanks for installing vntrs ", packageVersion("vntrs"), ", happy search!\n",
-    "Type 'citation(\"vntrs\")' for citing the package in publications.")
+#' @noRd
+#' @importFrom utils packageVersion
+
+.onAttach <- function(lib, pkg) {
+  msg <- paste0(
+    "Thanks for using {vntrs} ", utils::packageVersion("vntrs"),
+    ", happy search!\n", "Type 'citation(\"vntrs\")' for citing this package."
+  )
   packageStartupMessage(msg)
   invisible()
 }
